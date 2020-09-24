5704b348436927bd2a1cae36cee6f6919d2f5359
Tae <taehunk@uvic.ca>
2020-09-23 19:44:50 -0700

    Initial Commit

---
+++ src/main/java/birds/tinker/main/Main.kt
@@ -0,0 +1,28 @@
+package birds.tinker.main
+
+import discord4j.core.DiscordClient
+import discord4j.core.event.domain.message.MessageCreateEvent
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.reactive.awaitSingle
+import kotlinx.coroutines.reactor.mono
+
+fun main(args : Array<String>){
+    val token = args[0]
+    val client = DiscordClient.create(token)
+
+    client.withGateway {
+        mono {
+            it.on(MessageCreateEvent::class.java)
+                .asFlow()
+                .collect {
+                    val message = it.message
+                    if (message.content == "!ping") {
+                        val channel = message.channel.awaitSingle()
+                        channel.createMessage("Pong!").awaitSingle()
+                    }
+                }
+        }
+    }
+        .block()
+}
